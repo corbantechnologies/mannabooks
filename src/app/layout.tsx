dc1dec7b0cfbffc4887d43e3c1e2effc5933c869
Dalienst <oduordalienst@gmail.com>
2026-07-03 21:11:20 +0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -53,6 +54,7 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
       <body className="font-sans min-h-screen flex flex-col bg-white text-black">
         {children}
+        <Analytics />
       </body>
     </html>
   );
